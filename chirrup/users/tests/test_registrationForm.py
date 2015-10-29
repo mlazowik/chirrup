4d3ac963342e755d38d6c1f8adec1b548d546b55
Michał Łazowik <mlazowik@gmail.com>
2015-10-29 15:26:54 +0100

    More registration form testing

--- chirrup/users/tests/test_registrationForm.py
+++ chirrup/users/tests/test_registrationForm.py
@@ -3,13 +3,25 @@ from users.forms import RegistrationForm
 
 
 class TestRegistrationForm(TestCase):
-    def test_email(self):
-        data = {'username': 'test', 'email': 'u@u.com', 'password1': 'p', 'password2': 'p'}
-        form = RegistrationForm(data=data)
+    def setUp(self):
+        self.data = {'username': 'test', 'email': 'u@u.com', 'password1': 'p', 'password2': 'p'}
 
+    def test_email_is_saved(self):
+        form = RegistrationForm(data=self.data)
         self.assertTrue(form.is_valid())
 
         user = form.save()
 
         self.assertEqual(user.email, 'u@u.com')
 
+    def test_invalid_email_is_rejected(self):
+        self.data['email'] = 'u@u.u'
+        form = RegistrationForm(data=self.data)
+
+        self.assertFalse(form.is_valid())
+
+    def test_not_matching_passwords_are_rejected(self):
+        self.data['password1'] = 'pp'
+        form = RegistrationForm(data=self.data)
+
+        self.assertFalse(form.is_valid())
